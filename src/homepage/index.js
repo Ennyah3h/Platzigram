050f5e16257ff30c52152598bd4724048c85348f
hugo perez garcia <h3h__@hotmail.com>
2016-06-16 04:51:34 +0200

    Obteniendo una respuesta del servidor con superagent, Axios y Fetch API

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,33 +2,49 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
+var axios = require('axios');
 
-page('/', function (ctx, next) {
+page('/', header, loadPicturesFetch, function (ctx, next) {
   title('Platzigram');
   var main = document.getElementById('main-container');
 
-  var pictures = [
-    {
-      user: {
-        username: 'Ennyah3h',
-        avatar: 'avatar.jpeg'
-      },
-      url: 'office.jpg',
-      likes: 0,
-      liked: false,
-      createdAt: new Date().getTime()
-    },
-    {
-      user: {
-        username: 'Ennyah3h',
-        avatar: 'avatar.jpeg'
-      },
-      url: 'office.jpg',
-      likes: 1,
-      liked: true,
-      createdAt: new Date().setDate(new Date().getDate() - 10)
-    }
-  ];
-
-  empty(main).appendChild(template(pictures));
+  empty(main).appendChild(template(ctx.pictures));
 })
+// 
+// function loadPictures(ctx, next) {
+//   request
+//     .get('/api/pictures')
+//     .end(function (err, res) {
+//       if (err) return console.log(err);
+//       ctx.pictures = res.body;
+//       next();
+//     })
+// }
+//
+// function loadPicturesAxios(ctx, next) {
+//   axios
+//     .get('/api/pictures')
+//     .then(function (res) {
+//       ctx.pictures = res.data;
+//       next();
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     })
+// }
+
+function loadPicturesFetch(ctx, next) {
+  fetch('/api/pictures')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (pictures) {
+      ctx.pictures = pictures;
+      next()
+      .catch(function (err) {
+        console.log(err);
+      })
+    })
+}
